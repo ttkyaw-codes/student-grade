880b8bc393e2c8f36ba8edeaf598d92e470dbce6
Jaja <thanttunkyaw621@gmail.com>
2026-09-14 13:34:23 +0630

    Added Total Grades

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 name = input("Input name:")
 grades = [grade for grade in input("Input two grades separated by space:").split(" ")]
-avg_grade = 0
+total_grade = 0
 for grade in grades:
-    avg_grade += int(grade)
-avg_grade = avg_grade/len(grades)
+    total_grade += int(grade)
+avg_grade = total_grade/len(grades)
     
-print(f"Name: {name}\nAverage Grade: {avg_grade}")
+print(f"Name: {name}\nAverage Grade: {avg_grade}\nTotal Grade: {total_grade}")
