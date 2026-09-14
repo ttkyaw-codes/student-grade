e01ba4600d13d2ca308f5c049836a171b04949a2
Jaja <thanttunkyaw621@gmail.com>
2026-09-14 13:52:40 +0630

    Revert changes

--- app.py
+++ app.py
@@ -4,12 +4,5 @@ total_grade = 0
 for grade in grades:
     total_grade += int(grade)
 avg_grade = total_grade/len(grades)
-
-if avg_grade >= 40:
-    status = "Passed"
-elif avg_grade > 0 and avg_grade < 40:
-    status = "Failed"
-else:
-    status = "Error"
     
-print(f"Name: {name}\nAverage Grade: {avg_grade}\nStaus: {status}\nTotal Grade: {total_grade}")
+print(f"Name: {name}\nAverage Grade: {avg_grade}\nTotal Grade: {total_grade}")
